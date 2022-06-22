d4f325a2da27b89e3917eb0b9d74feb07a5d2d51
s7manth <syalamarty002@gmail.com>
2022-06-22 09:58:24 +0530

    fix data router reference function for read

--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -4,27 +4,27 @@ const dataController = require('../controllers/dataController');
 const router = express.Router();
 
 router.post('/user', dataController.createUser);
-router.get('/user', data.dataController.getUser);
+router.get('/user', dataController.getUser);
 router.patch('/user', dataController.updateUser);
 router.delete('/user', dataController.deleteUser);
 
 router.post('/merchant', dataController.createMerchant);
-router.get('/merchant', data.dataController.getMerchant);
+router.get('/merchant', dataController.getMerchant);
 router.patch('/merchant', dataController.updateMerchant);
 router.delete('/merchant', dataController.deleteMerchant);
 
 router.post('/offer', dataController.createOffer);
-router.get('/offer', data.dataController.getOffer);
+router.get('/offer', dataController.getOffer);
 router.patch('/offer', dataController.updateOffer);
 router.delete('/offer', dataController.deleteOffer);
 
 router.post('/reward', dataController.createReward);
-router.get('/reward', data.dataController.getReward);
+router.get('/reward', dataController.getReward);
 router.patch('/reward', dataController.updateReward);
 router.delete('/reward', dataController.deleteReward);
 
 router.post('/payment', dataController.createPayment);
-router.get('/payment', data.dataController.getPayment);
+router.get('/payment', dataController.getPayment);
 router.patch('/payment', dataController.updatePayment);
 router.delete('/payment', dataController.deletePayment);
 
